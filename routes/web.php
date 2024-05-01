dc6000f20c34bbf094b750307592afab6b13fb42
AdrianIchiro <adrianfahren10@gmail.com>
2024-05-01 14:50:08 +0700

    grouping middleware

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,27 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-// Get Method
+
+// Default
 Route::view('/', 'index', ['title' => 'Home'])->name('home');
-Route::view('/register', 'register-form', ['title' => 'Register'])->name('register');
-Route::view('/login', 'login-form', ['title' => 'Login'])->name('login');
-Route::view('/posts', 'posts', ['title' => 'Blog'])->name('posts')->middleware('auth');
-Route::view('/profile', 'profile', ['title' => 'Profile'])->name('profile');
 
+// Auth group
+Route::middleware('auth')->group(function() {
+    // Get Method
+    Route::view('/posts', 'posts', ['title' => 'Blog'])->name('posts');
+    Route::view('/profile', 'profile', ['title' => 'Profile'])->name('profile');
 
-// Post Method
-Route::post('/register', [AuthController::class, 'register'])->name('register-action');
-Route::post('/login', [AuthController::class, 'login'])->name('login-action');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    // Post Method
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+// Guest group
+Route::middleware('guest')->group(function() {
+    // Get Method
+    Route::view('/register', 'register-form', ['title' => 'Register'])->name('register');
+    Route::view('/login', 'login-form', ['title' => 'Login'])->name('login');
+
+    // Post Method
+    Route::post('/register', [AuthController::class, 'register'])->name('register-action');
+    Route::post('/login', [AuthController::class, 'login'])->name('login-action');
+});
